b78bf6bf258ae24d06f6c1444ea881ee40a38e00
Maria Monica <69426325+learnrpa2020@users.noreply.github.com>
2020-08-09 21:27:50 +0530

    Update datastructures.py
    corrected the syntax for augmented assignmentoperator

--- datastructures.py
+++ datastructures.py
@@ -10,7 +10,7 @@ for i in range(amazon_products_len):
     
     #value=input()
     amazon_products.insert(i,input())
-    i=+1
+    i+=1
     #print(amazon_products)
 print(amazon_products)
 
@@ -22,7 +22,7 @@ for i in range(amazon_products_len):
     key = input()
     value = int(input())
     amazon_products.update({key:value})
-    i=+1
+    i+=1
 print(amazon_products)
 print(amazon_products.items())
 print(amazon_products.keys())
