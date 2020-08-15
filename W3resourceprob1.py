e1b591fe8a2f1add57d8d19529125aa2f043b1ca
Maria Monica <69426325+learnrpa2020@users.noreply.github.com>
2020-08-15 19:23:38 +0530

    Update W3resourceprob1.py

--- W3resourceprob1.py
+++ W3resourceprob1.py
@@ -11,3 +11,56 @@ Twinkle, twinkle, little star,
 '''
 
 print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are")
+
+
+n=int(input('Enter the number'))
+if n%2 != 0:
+    print('Weird')
+else:
+    if n in range(2,6):
+        print('Not Weird')
+    elif n in range(6,21):
+        print('Weird')
+    else:
+        print('Not Weird')
+
+
+'''Kevin and Stuart want to play the 'The Minion Game'.
+
+Game Rules
+
+Both players are given the same string, .
+Both players have to make substrings using the letters of the string .
+Stuart has to make words starting with consonants.
+Kevin has to make words starting with vowels.
+The game ends when both players have made all possible substrings.
+
+Scoring
+A player gets +1 point for each occurrence of the substring in the string .
+
+For Example:
+String  = BANANA
+Kevin's vowel beginning word = ANA
+Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.'''
+
+
+word=input('Enter the word')
+a=len(word)
+li =[]
+li1=[]
+stuart=0
+kevin=0
+for i in range(a+1):
+    for j in range(a+1):
+        if i!=j and j>i:
+            if word[i][0] in ['A','E','I','O','U']:
+                li.append(word[i:j:])
+                kevin=len(li)
+            else:
+                li1.append(word[i:j:])
+                stuart=len(li1)
+                
+print(kevin)
+print(stuart)
+
+
